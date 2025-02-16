c04e959662e37f9c264785ef5fc1bde150bc33e5
Adam <goldiron2103@gmail.com>
2025-02-16 21:45:27 +0700

    Tugas OOP pertemuan ke-satu

--- Pertemuan_1/OOP.java
+++ Pertemuan_1/OOP.java
@@ -4,15 +4,16 @@ import javax.swing.*;
 
 class TugasOOP_1 {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null,"Tugas OOP pertama!!!","Tugas",3);
-        JOptionPane.showInputDialog(null, "Siapa nama kamu?", "Masukkan nama", 3);
-    }
-}
-
-class TugasOOP_2 {
-    public static void main(String[] args) {
         System.out.println("Belajar JAVA");
         System.out.println("Sangat mudah sekali");
         System.out.println("dan sangat menyenangkan");
     }
 }
+
+class TugasOOP_2 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Tugas OOP pertama!!!","Tugas",3);
+        JOptionPane.showInputDialog(null, "Siapa nama kamu?", "Masukkan nama", 3);
+    }
+}
+
